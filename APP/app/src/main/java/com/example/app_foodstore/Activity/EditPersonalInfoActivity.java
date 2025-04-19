bf30fb0c4ccf6d322c4898dadc4785aeaeb9e6b0
DatHocVietCode <td13052004@gmail.com>
2025-04-19 23:19:01 +0700

    update choose image from storage

--- APP/app/src/main/java/com/example/app_foodstore/Activity/EditPersonalInfoActivity.java
+++ APP/app/src/main/java/com/example/app_foodstore/Activity/EditPersonalInfoActivity.java
@@ -1,16 +1,115 @@
 package com.example.app_foodstore.Activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.app_foodstore.R;
 
+import java.io.IOException;
+
 public class EditPersonalInfoActivity extends AppCompatActivity {
+    ImageButton btnChooseImage;
+    ImageView imgAvatar;
+    Uri mUri;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_personal__info);
+
+        btnChooseImage = findViewById(R.id.edit_PI_btnChooseImage);
+        imgAvatar = findViewById(R.id.edit_PI_avatar);
+        btnChooseImage.setOnClickListener(v -> CheckPermission());
     }
+
+    public static final int MY_REQUEST_CODE=100;
+    public static String[] storge_permissions = {
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    public static String[] storge_permissions_33 = {
+            android.Manifest.permission.READ_MEDIA_IMAGES,
+            android.Manifest.permission.READ_MEDIA_AUDIO,
+            android.Manifest.permission.READ_MEDIA_VIDEO
+    };
+    public static String[] permissions() {
+        String[] p;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            p = storge_permissions_33;
+        } else {
+            p = storge_permissions;
+        }
+        return p;
+    }
+    private void CheckPermission() {
+        if (Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
+            openGallery();
+            return;
+        }
+        if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            openGallery();
+        }else{
+            //ActivityCompat.requestPermissions(UploadImageActivity.this,permissions(),MY_REQUEST_CODE);
+            //hoặc
+            requestPermissions(permissions(),MY_REQUEST_CODE);
+        }
+    }
+    //Hàm xử lý lấy ảnh
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        mActivityResultLauncher.launch(Intent.createChooser(intent, "Select Picture"));
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_REQUEST_CODE) {
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery();
+            }
+        }
+    }
+    private ActivityResultLauncher<Intent> mActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Log.e("logd",  "onActivityResult");
+                    if(result.getResultCode()== Activity.RESULT_OK){
+                        //request code
+                        Intent data = result.getData();
+                        if(data==null){
+                            return;
+                        }
+                        Uri uri = data.getData();
+                        mUri = uri;
+                        try {
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                            imgAvatar.setImageBitmap(bitmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
 }
