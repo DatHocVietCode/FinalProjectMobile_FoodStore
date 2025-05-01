a76c1492b0a730557c00ef3a0a0d7d3dea71c7f3
DatHocVietCode <td13052004@gmail.com>
2025-05-02 00:02:33 +0700

    fix #1

--- APP/app/src/main/java/com/example/app_foodstore/APIService/Constant.java
+++ APP/app/src/main/java/com/example/app_foodstore/APIService/Constant.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class Constant {
     // Dùng cho gia3 lập
     //public static final String BASE_URL = "http://10.0.2.2:8081/";
-    // Dùng cho android
+    // Dùng cho thiết bị thật chung mạng
     public static final String BASE_URL = "http://192.168.1.23:8081/";
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
