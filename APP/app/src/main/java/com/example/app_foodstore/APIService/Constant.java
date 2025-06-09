ee7cb67986347e6d3b7d335de133f8b9b19806f8
DatHocVietCode <td13052004@gmail.com>
2025-06-09 20:50:27 +0700

    reconfig

--- APP/app/src/main/java/com/example/app_foodstore/APIService/Constant.java
+++ APP/app/src/main/java/com/example/app_foodstore/APIService/Constant.java
@@ -7,8 +7,8 @@ public class Constant {
     // Dùng cho gia3 lập
 //    public static final String BASE_URL = "http://10.0.2.2:8080/";
     // Dùng cho thiết bị thật chung mạng
-//    public static final String BASE_URL = "http://196.169.3.204:8081/";
-    public static final String BASE_URL = "http://196.169.2.70:8080/";
+    public static final String BASE_URL = "http://192.168.1.106:8081/";
+    //public static final String BASE_URL = "http://196.169.2.70:8080/";
     // Render
     //public static final String BASE_URL = "https://backend-foodapp-latest.onrender.com/";
     public static  final String IMG_URL = BASE_URL + "uploads/";
