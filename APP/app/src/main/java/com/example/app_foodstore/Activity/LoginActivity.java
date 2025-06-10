f2e1b829befebb70b4e6511540c0f48eabd5bc7f
DatHocVietCode <td13052004@gmail.com>
2025-06-10 22:01:56 +0700

    minor changes

--- APP/app/src/main/java/com/example/app_foodstore/Activity/LoginActivity.java
+++ APP/app/src/main/java/com/example/app_foodstore/Activity/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,6 +20,7 @@ import com.example.app_foodstore.ViewModel.LoginViewModel;
 import com.google.android.material.button.MaterialButton;
 public class LoginActivity extends AppCompatActivity {
     private LoginViewModel loginViewModel;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
